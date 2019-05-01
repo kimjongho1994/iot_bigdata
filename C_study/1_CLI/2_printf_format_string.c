8fecf40387ea393e3578e45d6d2842cbb842677e
JongHo Kim <you@example.com>
2019-05-01 10:52:08 +0900

    수정

---
+++ C_study/1_CLI/2_printf_format_string.c
@@ -0,0 +1,19 @@
+/* 2_printf_format_string.c */
+/* */ /* <== 주석 (comment) */
+/* 설명1 <== Old style(ANSI 99) comment */
+// 설명2 <== New style(ANSI 89) comment
+
+/* 설명 1
+ * 설명 2
+ * 설명 3
+*/
+// 설명 1
+// 설명 2
+// 설명 3
+
+#include <stdio.h>
+
+void main(){
+	printf("나는 사과를 1개 먹었다.");
+	
+
